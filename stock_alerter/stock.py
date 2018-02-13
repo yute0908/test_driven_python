9f9641e49db062c76a3de762450ef35107fcb245
yute0908 <linsante@gmail.com>
2018-02-14 07:36:01 +0800

    chapter2 test refactor

--- stock_alerter/stock.py
+++ stock_alerter/stock.py
@@ -1,4 +1,17 @@
 class Stock:
     def __init__(self, symbol):
         self.symbol = symbol
-        self.price = None
+        self.price_history = []
+
+    def update(self, timestamp, price):
+        if price < 0:
+            raise ValueError("price should not be negative")
+        self.price_history.append(price)
+
+    @property
+    def price(self):
+        return self.price_history[-1] \
+            if self.price_history else None
+
+    def is_increasing_trend(self):
+        return self.price_history[-3] < self.price_history[-2] < self.price_history[-1]
---
+++ stock_alerter/tests/test_test_order.py
@@ -0,0 +1,53 @@
+import unittest
+
+'''
+def setUpModule():
+    print("setUpModule")
+
+
+def tearDownModule():
+    print("tearDownModule")
+
+
+class Class1Test(unittest.TestCase):
+
+    @classmethod
+    def setUpClass(cls):
+        print("  setUpClass")
+
+    @classmethod
+    def tearDownClass(cls):
+        print("  tearDownClass")
+
+    def setUp(self):
+        print("       setUp")
+
+    def tearDown(self):
+        print("       tearDown")
+
+    def test_1(self):
+        print("         class 1 test 1")
+
+    def test_2(self):
+        print("         class 1 test 2")
+
+
+class Class2Test(unittest.TestCase):
+
+    @classmethod
+    def setUpClass(cls):
+        print("  setUpClass2")
+
+    @classmethod
+    def tearDownClass(cls):
+        print("  tearDownClass2")
+
+    def setUp(self):
+        print("       setUp2")
+
+    def tearDown(self):
+        print("       tearDown2")
+
+    def test_1(self):
+        print("         class 2 test 1")
+'''
