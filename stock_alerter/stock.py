a9fcb05156158a707170b942813db9aac3ffea2f
yute0908 <linsante@gmail.com>
2018-03-17 16:39:09 +0800

    Move method to class

--- stock_alerter/stock.py
+++ stock_alerter/stock.py
@@ -5,8 +5,6 @@ from enum import Enum
 
 from stock_alerter.timeseries import TimeSeries
 
-PriceEvent = collections.namedtuple("PriceEvent", ["timestamp", "price"])
-
 
 class StockSignal(Enum):
     buy = 1
@@ -20,13 +18,11 @@ class Stock:
 
     def __init__(self, symbol):
         self.symbol = symbol
-        self.price_history = []
         self.history = TimeSeries()
 
     def update(self, timestamp, price):
         if price < 0:
             raise ValueError("price should not be negative")
-        bisect.insort_left(self.price_history, PriceEvent(timestamp, price))
         self.history.update(timestamp, price)
 
     @property
@@ -40,19 +36,9 @@ class Stock:
         return self.history[-3].value < self.history[-2].value < self.history[-1].value
 
     def get_crossover_signal(self, on_date):
-        closing_price_list = []
+
         NUM_DAYS = self.LONG_TERM_TIMESPAN + 1
-        for i in range(NUM_DAYS):
-            chk = on_date.date() - timedelta(i)
-            for price_event in reversed(self.price_history):
-                if price_event.timestamp.date() > chk:
-                    pass
-                if price_event.timestamp.date() == chk:
-                    closing_price_list.insert(0, price_event)
-                    break
-                if price_event.timestamp.date() < chk:
-                    closing_price_list.insert(0, price_event)
-                    break
+        closing_price_list = self.history.get_closing_price_list(on_date, NUM_DAYS)
 
         # Return NEUTRAL signal
         if len(closing_price_list) < NUM_DAYS:
@@ -63,10 +49,10 @@ class Stock:
         short_term_series = closing_price_list[-self.SHORT_TERM_TIMESPAN:]
         prev_short_term_series = closing_price_list[-self.SHORT_TERM_TIMESPAN - 1:-1]
 
-        long_term_ma = sum([update.price for update in long_term_series]) / self.LONG_TERM_TIMESPAN
-        short_term_ma = sum([update.price for update in short_term_series]) / self.SHORT_TERM_TIMESPAN
-        prev_long_term_ma = sum([update.price for update in prev_long_term_series]) / self.LONG_TERM_TIMESPAN
-        prev_short_term_ma = sum([update.price for update in prev_short_term_series]) / self.SHORT_TERM_TIMESPAN
+        long_term_ma = sum([update.value for update in long_term_series]) / self.LONG_TERM_TIMESPAN
+        short_term_ma = sum([update.value for update in short_term_series]) / self.SHORT_TERM_TIMESPAN
+        prev_long_term_ma = sum([update.value for update in prev_long_term_series]) / self.LONG_TERM_TIMESPAN
+        prev_short_term_ma = sum([update.value for update in prev_short_term_series]) / self.SHORT_TERM_TIMESPAN
 
         # BUY signal
         if self._is_crossover_below_to_above(prev_short_term_ma, prev_long_term_ma, short_term_ma, long_term_ma):
--- stock_alerter/timeseries.py
+++ stock_alerter/timeseries.py
@@ -1,5 +1,6 @@
 import bisect
 import collections
+from datetime import timedelta
 
 Update = collections.namedtuple("Update", ["timestamp", "value"])
 
@@ -13,3 +14,18 @@ class TimeSeries:
 
     def __getitem__(self, index):
         return self.series[index]
+
+    def get_closing_price_list(self, on_date, num_days):
+        closing_price_list = []
+        for i in range(num_days):
+            chk = on_date.date() - timedelta(i)
+            for price_event in reversed(self.series):
+                if price_event.timestamp.date() > chk:
+                    pass
+                if price_event.timestamp.date() == chk:
+                    closing_price_list.insert(0, price_event)
+                    break
+                if price_event.timestamp.date() < chk:
+                    closing_price_list.insert(0, price_event)
+                    break
+        return closing_price_list
