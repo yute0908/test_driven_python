92e325721b55c207d5378b2e7941531f981ff35d
yute0908 <linsante@gmail.com>
2018-03-24 14:31:43 +0800

    Using the mocking framework.

--- stock_alerter/tests/test_event.py
+++ stock_alerter/tests/test_event.py
@@ -1,30 +1,22 @@
 import unittest
+from unittest import mock
 
 from stock_alerter.event import Event
 
 
-class Mock:
-
-    def __init__(self):
-        self.called = False
-        self.params = ()
-
-    def __call__(self, *args, **kwargs):
-        self.called = True
-        self.params = (args, kwargs)
-
 
 class EventTest(unittest.TestCase):
     def test_a_listener_is_notified_when_an_event_is_raised(self):
-        listener = Mock()
+        listener = mock.Mock()
         event = Event()
         event.connect(listener)
         event.fire()
         self.assertTrue(listener.called)
 
     def test_a_listener_is_passed_right_parameters(self):
-        listener = Mock()
+        listener = mock.Mock()
         event = Event()
         event.connect(listener)
         event.fire(5, shape="square")
-        self.assertEqual(((5,), {"shape": "square"}), listener.params)
+        """listener.assert_called_with(5, shape="square")"""
+        listener.assert_has_calls([mock.call(5, shape="square")])
