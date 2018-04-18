68eb87cc7b7b623f84b832089ee2f5d18b5283f5
dereklin <dereklin@yahoo-inc.com>
2018-04-18 13:19:59 +0800

    Use default values for parameters.

--- stock_alerter/tests/test_legacy.py
+++ stock_alerter/tests/test_legacy.py
@@ -4,9 +4,15 @@ from unittest import mock
 from stock_alerter.legacy import AlertProcessor
 
 
+@mock.patch("builtins.print")
 class AlertProcessorTest(unittest.TestCase):
-    @mock.patch("builtins.print")
     def test_processor_characterization_1(self, mock_print):
         AlertProcessor()
+        mock_print.assert_has_calls(
+            [mock.call("AAPL", 8), mock.call("GOOG", 15), mock.call("AAPL", 10), mock.call("GOOG", 21)])
+
+    def test_processor_characterization_2(self, mock_print):
+        processor = AlertProcessor(autorun=False)
+        processor.run()
         mock_print.assert_has_calls(
             [mock.call("AAPL", 8), mock.call("GOOG", 15), mock.call("AAPL", 10), mock.call("GOOG", 21)])
