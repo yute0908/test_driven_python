779465cfee98950d8978e7a8753c4d5c480d5199
yute0908 <linsante@gmail.com>
2018-05-11 07:27:37 +0800

    Extract the method and stub.

--- stock_alerter/legacy.py
+++ stock_alerter/legacy.py
@@ -14,22 +14,27 @@ class AlertProcessor:
         rule_1 = PriceRule("GOOG", lambda stock: stock.price > 10)
         rule_2 = PriceRule("AAPL", lambda stock: stock.price > 5)
         self.exchange["GOOG"].updated.connect(
-            lambda stock: print(stock.symbol, stock.price) \
-                          if rule_1.matches(self.exchange) else None)
+            lambda stock: self.print_action(stock, rule_1))
         self.exchange["AAPL"].updated.connect(
-            lambda stock: print(stock.symbol, stock.price) \
-                          if rule_2.matches(self.exchange) else None)
+            lambda stock: self.print_action(stock, rule_2))
         if autorun:
             self.run()
 
+    def print_action(self, stock, rule):
+        print(stock.symbol, stock.price) \
+            if rule.matches(self.exchange) else None
+
     def run(self):
+        updates = self.parse_file()
+        self.do_updates(updates)
+
+    def parse_file(self):
         updates = []
         with open("updates.csv", "r") as fp:
             for line in fp.readlines():
                 symbol, timestamp, price = line.split(",")
-                updates.append((symbol, datetime.strptime(timestamp, "%Y-%m-%dT%H:%M:%S.%f"),int(price)))
-
-        self.do_updates(updates)
+                updates.append((symbol, datetime.strptime(timestamp, "%Y-%m-%dT%H:%M:%S.%f"), int(price)))
+        return updates
 
     def do_updates(self, updates):
         for symbol, timestamp, price in updates:
