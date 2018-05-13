c0aa975630ad3524157d5afa4c15a62917175f80
yute0908 <linsante@gmail.com>
2018-05-14 07:44:34 +0800

    Using teat class equality

--- stock_alerter/tests/test_timeseries.py
+++ stock_alerter/tests/test_timeseries.py
@@ -18,3 +18,17 @@ class TimeSeriesTest(unittest.TestCase):
         series.update(datetime(2014, 3, 10), 5)
         on_date = datetime(2014, 3, 9)
         self.assertEqual([], series.get_closing_price_list(on_date, 1))
+
+    def assert_has_price_history(self, price_list, series):
+        for index, expected_price in enumerate(price_list):
+            actual_price = series[index].value
+            if actual_price != expected_price:
+                raise self.failureException("[%d]: %d != %d".format(index, expected_price, actual_price))
+
+
+class TimeSeriesEqualityTest(TimeSeriesTest):
+    def test_timeseries_price_history(self):
+        series = TimeSeries()
+        series.update(datetime(2014, 3, 10), 5)
+        series.update(datetime(2014, 3, 11), 15)
+        self.assert_has_price_history([5, 15], series)
