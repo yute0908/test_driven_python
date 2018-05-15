4d2cec2543193e3824d09b78faa5032c1822c705
yute0908 <linsante@gmail.com>
2018-05-16 07:36:46 +0800

    First doctest.

--- stock_alerter/stock.py
+++ stock_alerter/stock.py
@@ -30,6 +30,34 @@ class Stock:
 
     @property
     def price(self):
+        """Returns the current price of the Stock
+
+        >>> from datetime import datetime
+        >>> stock = Stock("GOOG")
+        >>> stock.update(datetime(2011, 10, 3), 10)
+        >>> stock.price
+        10
+
+        The method will return the latest price by timestamp, so even if updates are out of order, it will return the latest one
+
+         >>> stock = Stock("GOOG")
+         >>> stock.update(datetime(2011, 10, 3), 10)
+
+        Now, let us do an update with a date that is earlier than the previous one
+
+        >>> stock.update(datetime(2011, 10, 2), 5)
+
+        And the method still returns the latest price
+
+        >>> stock.price
+        10
+
+        If there are no updates, then the method returns None
+
+        >>> stock = Stock("GOOG")
+        >>> print(stock.price)
+        None
+        """
         try:
             return self.history[-1].value
         except IndexError:
@@ -61,3 +89,7 @@ class Stock:
         prev_date = on_date - timedelta(1)
         return (ma.value_on(prev_date) < reference_ma.value_on(prev_date) and ma.value_on(
             on_date) > reference_ma.value_on(on_date))
+
+if __name__ == "__main__":
+    import doctest
+    doctest.testmod()
